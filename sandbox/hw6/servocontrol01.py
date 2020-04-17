c33ecb43d8fde53f8623be6c24fc4f8637453550
Kevin Dong <kevindong80@gmail.com>
2020-04-17 14:39:18 -0400

    Added new looping structure to open and close claw in servocontrol01.py

--- sandbox/hw6/servocontrol01.py
+++ sandbox/hw6/servocontrol01.py
@@ -1,65 +1,121 @@
+'''
+Copyright (c) 2020 Hao Da (Kevin) Dong, Krithika Govindaraj
+@file       servocontrol01.py
+@date       2020/02/01
+@brief      Records a video of the Baron's claw movements with PWM duty cycle
+@license    This project is released under the BSD-3-Clause license.
+'''
+
+import RPi.GPIO as GPIO
 import cv2
-import numpy as np
-import Rpi.GPIO as GPIO
 import time
-import matplotlib.pyplt as plt
 
-	# Create a VideoCapture object and read from input file
-	# If the input is the camera, pass 0 instead of the video file name
-cap = cv2.VideoCapture(0)
-out = cv2.VideoWriter('outpy.avi',cv2.VideoWriter_fourcc('M','J','P','G'), 10, (frame_width,frame_height))
 
+# Choose whether or not to save the output video
+saveVideo = False
+resolution = (1280, 720)
+
+ # Define video codec and output file if video needs to be saved
+if (saveVideo == True):
+	fourcc = cv2.VideoWriter_fourcc(*'mp4v')
+	# 720p 30fps video
+	out = cv2.VideoWriter('CubeOverlayOutput.mp4', fourcc, 30, resolution)
+
+# Claw parameters
+clawPin = 36
+closePWM = 5.5
+openPWM = 9
+
+# Setup RPi GPIO pins
 GPIO.setmode(GPIO.BOARD)
-GPIO.setup(36,GPIO.OUT)
-pwm = GPIO.PWM(36,50)
+GPIO.setup(clawPin, GPIO.OUT)
+claw = GPIO.PWM(clawPin, 50)		# Set PWM to 50 Hz
 
-def get_frame(duty_cycle):
+# Start claw in closed position
+claw.start(closePWM)
 
-	ret, frame = cap.read()
-	if ret == True:
+# Create video capture object
+cap = cv2.VideoCapture(0)
 
+# Open and close claw until user specifies an exit
+while (cap.isOpened()): 
+	exitCondition = 0
+
+	# Increase duty cycle to open claw
+	for i in range(closePWM, openPWM, 0.5):
+
+		ret, frame = cap.read()
+		if ret == True:
+			claw.ChangeDutyCycle(i)
+			time.sleep(0.5)
+			
+			# Display the frame and duty cycle
+			cv2.putText(frame, 'Duty Cycle: ' + str(i) + '%', (10,30), \
+						cv2.FONT_HERSHEY_SIMPLEX, 1, \
+						color=(0,0,255), thickness=3)
+			cv2.imshow('Frame', frame)
+
+
+			# Save frame to video if desired
+			if (saveVideo == True):
+				out.write(cv2.resize(frame, resolution))
+
+
+			# Exit loop if user presses 'q'
+			if cv2.waitKey(1) & 0xFF == ord('q'):
+				exitCondition = 1
+				break
+
+		# If the current frame is invalid, continue to the next one
+		else: 
+			continue
+
+	# Decrease duty cycle to close claw
+	for i in range(openPWM, closePWM, 0.5):
+
+		ret, frame = cap.read()
+		if ret == True:
+			claw.ChangeDutyCycle(i)
+			time.sleep(0.5)
+			
+			# Display the frame and duty cycle
+			cv2.putText(frame, 'Duty Cycle: ' + str(i) + '%', (10,30), \
+						cv2.FONT_HERSHEY_SIMPLEX, 1, \
+						color=(0,0,255), thickness=3)
+			cv2.imshow('Frame', frame)
+
+
+			# Save frame to video if desired
+			if (saveVideo == True):
+				out.write(cv2.resize(frame, resolution))
+
+
+			# Exit loop if user presses 'q'
+			if cv2.waitKey(1) & 0xFF == ord('q'):
+				exitCondition = 1
+				break
+
+		# If the current frame is invalid, continue to the next one
+		else: 
+			continue
 		
-		pwm.start(duty_cycle)
-		time.sleep(2)
-		
-
-	# Display the resulting frame
-		plt.text(0.5, 0.5, str(duty_cycle), fontsize=12)
-		plt.imshow('Frame',frame)
-		plt.show()
-		out.write(frame)
+		# Exit while loop if the user exited from inner for loops
+		if (exitCondition == 1):
+			break
 
 
-	# Press Q on keyboard to  exit
-		if cv2.waitKey(25) & 0xFF == ord('q'):
-		  break
-
-	# Break the loop
-	else: 
-		break
-
-
-
-################### MAIN #############################		
-
-	# Check if camera opened successfully
-if (cap.isOpened()== False): 
-print("Error opening video stream or file")
-
-# Read until video is completed
-while(cap.isOpened()):
-# Capture frame-by-frame
-	get_frame(5) % left 
-
-	get_frame(7.5) % center
-
-
-# When everything done, release the video capture object
+# Release video and file object handles
 cap.release()
-pwm.stop()
+if (saveVideo == True):
+	out.release()
+
+print('Video and file handles closed')
+
+# Stop PWM signal and clean up GPIO
+claw.stop()
 GPIO.cleanup()
 
-# Closes all the frames
+# Close all windows
 cv2.destroyAllWindows()
 
 
