4a6b5458a870365859135608b75993a59c415578
root <root@raspberrypi>
2020-04-17 20:15:24 -0400

    Modified loop structure and video capture object buffer size in servocontrol01.py

--- sandbox/hw6/servocontrol01.py
+++ sandbox/hw6/servocontrol01.py
@@ -7,19 +7,20 @@ Copyright (c) 2020 Hao Da (Kevin) Dong, Krithika Govindaraj
 '''
 
 import RPi.GPIO as GPIO
+import numpy as np
 import cv2
 import time
 
 
 # Choose whether or not to save the output video
-saveVideo = False
+saveVideo = True
 resolution = (1280, 720)
 
  # Define video codec and output file if video needs to be saved
 if (saveVideo == True):
 	fourcc = cv2.VideoWriter_fourcc(*'mp4v')
 	# 720p 30fps video
-	out = cv2.VideoWriter('CubeOverlayOutput.mp4', fourcc, 30, resolution)
+	out = cv2.VideoWriter('output.mp4', fourcc, 1, resolution)
 
 # Claw parameters
 clawPin = 36
@@ -36,21 +37,30 @@ claw.start(closePWM)
 
 # Create video capture object
 cap = cv2.VideoCapture(0)
+cap.set(cv2.CAP_PROP_BUFFERSIZE, 1)
+
+# Create claw PWM duty cycle sequence
+openSequence = np.arange(closePWM, openPWM+0.5, 0.5)
+closeSequence = np.flip(openSequence)
+fullSequence = np.hstack((openSequence, closeSequence))
 
 # Open and close claw until user specifies an exit
 while (cap.isOpened()): 
 	exitCondition = 0
 
-	# Increase duty cycle to open claw
-	for i in range(closePWM, openPWM, 0.5):
+	# Run claw through the open/close sequence
+	for i in range(len(fullSequence)):
+		dutyCycle = fullSequence[i]
+		claw.ChangeDutyCycle(dutyCycle)
+		time.sleep(2)
 
 		ret, frame = cap.read()
-		if ret == True:
-			claw.ChangeDutyCycle(i)
-			time.sleep(0.5)
-			
+		if ret == True:			
+			# Flip the frame both horizontally and vertically
+			frame = cv2.flip(frame, -1)
+
 			# Display the frame and duty cycle
-			cv2.putText(frame, 'Duty Cycle: ' + str(i) + '%', (10,30), \
+			cv2.putText(frame, 'Duty Cycle: ' + str(dutyCycle) + '%', (10,30), \
 						cv2.FONT_HERSHEY_SIMPLEX, 1, \
 						color=(0,0,255), thickness=3)
 			cv2.imshow('Frame', frame)
@@ -70,38 +80,10 @@ while (cap.isOpened()):
 		else: 
 			continue
 
-	# Decrease duty cycle to close claw
-	for i in range(openPWM, closePWM, 0.5):
 
-		ret, frame = cap.read()
-		if ret == True:
-			claw.ChangeDutyCycle(i)
-			time.sleep(0.5)
-			
-			# Display the frame and duty cycle
-			cv2.putText(frame, 'Duty Cycle: ' + str(i) + '%', (10,30), \
-						cv2.FONT_HERSHEY_SIMPLEX, 1, \
-						color=(0,0,255), thickness=3)
-			cv2.imshow('Frame', frame)
-
-
-			# Save frame to video if desired
-			if (saveVideo == True):
-				out.write(cv2.resize(frame, resolution))
-
-
-			# Exit loop if user presses 'q'
-			if cv2.waitKey(1) & 0xFF == ord('q'):
-				exitCondition = 1
-				break
-
-		# If the current frame is invalid, continue to the next one
-		else: 
-			continue
-		
-		# Exit while loop if the user exited from inner for loops
-		if (exitCondition == 1):
-			break
+	# Exit while loop if the user exited from inner for loops
+	if (exitCondition == 1):
+		break
 
 
 # Release video and file object handles
