003c495c797fac197631d024de434ea619e0dee6
Kevin Dong <kevindong80@gmail.com>
2020-04-24 16:55:34 -0400

    Deleted hw6 folder from sandbox and added comments to encodercontrol01.py

--- sandbox/hw7/encodercontrol01.py
+++ sandbox/hw7/encodercontrol01.py
@@ -1,40 +1,54 @@
-import Rpi.GPIO as gpio
-import numpy as numpy
+'''
+Copyright (c) 2020 Hao Da (Kevin) Dong, Krithika Govindaraj
+@file       encodercontrol01.py
+@date       2020/04/01
+@brief      Encoder test to record 20 state changes in a text file
+@license    This project is released under the BSD-3-Clause license.
+'''
+
+import RPi.GPIO as gpio
+import numpy as np
 
 filename = "enc01.txt"
 states = []
 
 ##### Initialize GPIO pins ####
 def init():
+	# Set GPIO mode to board pin numbering
 	gpio.setmode(gpio.BOARD)
-	gpio.setup(12,gpio.IN, pull_up_down = gpio.PUD_UP)
+
+	# Set pull-up resistor since the encoder pin is open-drain 
+	gpio.setup(12, gpio.IN, pull_up_down = gpio.PUD_UP)
+
 
 def gameover():
 	gpio.cleanup()
 
+
 def saveToFile(filename, states):
 	f = open(filename, "w+")
 	for i in range(len(states)):
-		f.write(str(states[i])+"\n")
+		f.write(str(states[i]) + "\n")
 	f.close()
 
 
-########### Main Code #########
-init()
+if __name__ == '__main__':
+	init()
 
-counter = np.uint64(0)
-button = int(0)
+	counter = np.uint64(0)	# Counter to keep track of states
+	encoderState = int(0)	# Current encoder state
 
-while True:
+	while True:
+		# Update encoder state if it is different from pin input
+		if (int(gpio.input(12)) != int(encoderState)):
+			button = int(gpio.input(12))
+			states.append(button)	# Record state
+			counter += 1			# Update counter
+			print(counter)
 
-	if int(gpio.input(12) != int(button)):
-		button = int(gpio.input(12))
-		states.append(button)
-		counter += 1
-		print(counter)
-
-	if counter >= 20:
-		gameover()
-		saveToFile(filename, states)
-		print("Thanks for playing !")
-		break
+		# If 20 changes have been reached, save states to file and exit
+		if (counter >= 20):
+			gameover()
+			saveToFile(filename, states)
+			print("Thanks for playing!")
+			break
