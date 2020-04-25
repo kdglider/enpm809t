d87875ce097b8b9ed2b606924e8b2eb215479cb1
Kevin Dong <kevindong80@gmail.com>
2020-04-24 21:39:54 -0400

    Fixed syntax errors and changed loop structure in encodercontrol04.py

--- sandbox/hw7/encodercontrol04.py
+++ sandbox/hw7/encodercontrol04.py
@@ -1,9 +1,7 @@
-import Rpi.GPIO as gpio
+import RPi.GPIO as gpio
 import time
 import numpy as np
-
 import matplotlib.pyplot as plt
-
 
 
 statesRight = []
@@ -14,17 +12,17 @@ def init():
 	gpio.setmode(gpio.BOARD)
 	gpio.setup(31,gpio.OUT)  #IN1
  	gpio.setup(33, gpio.OUT) #IN2
- 	gpio.setup(35, gpio.out) #IN3
- 	gpio.setup(37, gpio.out) #IN4
+ 	gpio.setup(35, gpio.OUT) #IN3
+ 	gpio.setup(37, gpio.OUT) #IN4
 
  	gpio.setup(12, gpio.IN, pull_up_down = gpio.PUD_UP) # back right encoder
  	gpio.setup(7, gpio.IN, pull_up_down = gpio.PUD_UP) # front left encoder
 
 def gameover():
-	gpio.output(31, FALSE)
-	gpio.output(33, FALSE)
-	gpio.output(35, FALSE)
-	gpio.output(37, FALSE)
+	gpio.output(31, False)
+	gpio.output(33, False)
+	gpio.output(35, False)
+	gpio.output(37, False)
 	gpio.cleanup()
 
 def saveToFile(filename, states):
@@ -45,19 +43,19 @@ init()
 counterBR = np.uint64(0)
 counterFL = np.uint64(0)
 
-
 buttonBR = int(0)
 buttonFL = int(0)
 
 # Initialize pwm signal to control motor
 pwm1 = gpio.PWM(31,50) # BackRight
 pwm2 = gpio.PWM(37,50)
-val = 22
+val = 50
 pwm1.start(val)
 pwm2.start(val)
 time.sleep(0.1)
 
-for i in range(0, 100000):
+
+while (counterBR < 20 and counterFL < 20):
 	print("counterBR = ", counterBR,
 		  "counterFL = ", counterFL, 
 	      "BR state: ", gpio.input(12), 
@@ -66,38 +64,21 @@ for i in range(0, 100000):
 	if int(gpio.input(12) != int(buttonBR)):
 		button = int(gpio.input(12)) #holds the state
 		statesRight.append(button)
-		counter += 1
+		counterBR += 1
 
 	if int(gpio.input(7) != int(buttonFL)):
 		button = int(gpio.input(7)) #holds the state
 		statesLeft.append(button)
-		counter += 1
+		counterFL += 1
 		
 
-	if counterBR >= 20 and counterFL >= 20:
-		pwm1.stop()
-		pwm2.stop()
-		gameover()
-		saveToFile("enc03Right.txt", statesRight)
-		saveToFile("enc03Left.txt" statesLeft)
-		plotVal(statesRight)
-		plotVal(statesLeft)
-		print("Thanks for playing !")
-		break
+pwm1.stop()
+pwm2.stop()
+gameover()
+saveToFile("enc03Right.txt", statesRight)
+saveToFile("enc03Left.txt" statesLeft)
+plotVal(statesRight)
+plotVal(statesLeft)
+print("Thanks for playing !")
 
 
-	if counterBR >= 20:
-		pwm2.stop()
-		gameover()
-		saveToFile("enc03Right.txt", statesRight)
-		plotVal(statesRight)
-		print("Thanks for playing !")
-		break
-
-	if counterFL >= 20:
-		pwm1.stop()
-		gameover()
-		saveToFile("enc03Left.txt" statesLeft)
-		plotVal(statesLeft)
-		print("Thanks for playing !")
-		break
